c4c838653fa4984c06f5328d2557286f50eec0fc
Bhavya Giri <bhavya.giri44@gmail.com>
2022-09-27 09:01:51 +0530

    map

---
+++ pages/user.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const user = () => {
+  return (
+    <div className=''>
+        user
+        <Navbar/>
+    </div>
+  )
+}
+
+export default user
