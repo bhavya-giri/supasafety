1127c5e77e1d711997545c53f6457a8865ce7eb6
Ishaan Bedi <bediishaan6@gmail.com>
2022-09-27 20:15:04 +0530

    Supabase auth

--- components/EmergencyContact.js
+++ components/EmergencyContact.js
@@ -4,27 +4,27 @@ const EmergencyContact = () => {
   return (
     <div>
         <div>
-            <label class="sr-only" for="name">Name</label>
+            <label className="sr-only" htmlFor="name">Name</label>
             <input
-              class="w-full p-3 text-sm border-gray-200 rounded-lg"
+              className="w-full p-3 text-sm border-gray-200 rounded-lg"
               placeholder="Name"
               type="text"
               id="name"
             />
           </div>
           <div>
-              <label class="sr-only" for="phone">Phone</label>
+              <label className="sr-only" htmlFor="phone">Phone</label>
               <input
-                class="w-full p-3 text-sm border-gray-200 rounded-lg"
+                className="w-full p-3 text-sm border-gray-200 rounded-lg"
                 placeholder="Phone Number"
                 type="tel"
                 id="phone"
               />
             </div>
             <div>
-              <label class="sr-only" for="phone">Your Relation</label>
+              <label className="sr-only" htmlFor="phone">Your Relation</label>
               <input
-                class="w-full p-3 text-sm border-gray-200 rounded-lg"
+                className="w-full p-3 text-sm border-gray-200 rounded-lg"
                 placeholder="Your Relation"
                 type="text"
                 id="relation"
